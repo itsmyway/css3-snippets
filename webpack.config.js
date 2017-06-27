b08d053b5b00c9d7de0e9d29c3d0924a25bc5abf
anupriya <anupriya.hari@gmail.com>
2017-06-27 12:57:31 -0700

    Adding rough flexbox code

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack'),
+    path = require('path');
+
+module.exports = {
+    debug: true,
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [{
+          test: /\.scss$/,
+          loader: "css-loader!sass-loader"
+        }]
+    }
+};
